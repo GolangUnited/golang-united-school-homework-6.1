ae853e9a5f563671ca0ad27481d2d696aaea3763
IrakliGiorgadze <ika.giorgadze@gmail.com>
2022-06-01 15:50:33 +0400

    almost final

--- box.go
+++ box.go
@@ -1,8 +1,8 @@
 package golang_united_school_homework
 
-import "errors"
-
-var outOfIndexErr = errors.New("out of index")
+import (
+	"errors"
+)
 
 // box contains list of shapes and able to perform operations on them
 type box struct {
@@ -13,7 +13,6 @@ type box struct {
 // NewBox creates new instance of box
 func NewBox(shapesCapacity int) *box {
 	return &box{
-		shapes:         make([]Shape, 0),
 		shapesCapacity: shapesCapacity,
 	}
 }
@@ -21,76 +20,100 @@ func NewBox(shapesCapacity int) *box {
 // AddShape adds shape to the box
 // returns the error in case it goes out of the shapesCapacity range.
 func (b *box) AddShape(shape Shape) error {
-	if len(b.shapes) >= b.shapesCapacity {
-		return errors.New("can't add more shapes")
+
+	quantity := len(b.shapes)
+
+	if quantity < b.shapesCapacity {
+		b.shapes = append(b.shapes, shape)
+		return nil
+	} else {
+		return errors.New("too much arguments")
 	}
-	b.shapes = append(b.shapes, shape)
-	return nil
+
 }
 
 // GetByIndex allows getting shape by index
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
-	if i < 0 || i >= len(b.shapes) {
-		return nil, outOfIndexErr
+	if b.shapesCapacity < i {
+		return nil, errors.New("there are fewer figures in the box than the index")
 	}
-	return b.shapes[i], nil
+	if i >= len(b.shapes) || i < 0 {
+		return nil, errors.New("the element does not exist")
+	} else {
+		return b.shapes[i], nil
+	}
+
 }
 
 // ExtractByIndex allows getting shape by index and removes this shape from the list.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
-	if i < 0 || i >= len(b.shapes) {
-		return nil, outOfIndexErr
+	if b.shapesCapacity < i {
+		return nil, errors.New("there are fewer figures in the box than the index")
 	}
-	result := b.shapes[i]
-	copy(b.shapes[i:], b.shapes[i+1:])
-	b.shapes[len(b.shapes)-1] = nil
-	b.shapes = b.shapes[:len(b.shapes)-1]
-	return result, nil
+	if i >= len(b.shapes) || i < 0 {
+		return nil, errors.New("the element does not exist")
+	} else {
+		result := b.shapes[i]
+		b.shapes = append(b.shapes[0:i], b.shapes[i+1:]...)
+		return result, nil
+	}
+
 }
 
 // ReplaceByIndex allows replacing shape by index and returns removed shape.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
-	if i < 0 || i >= len(b.shapes) {
-		return nil, outOfIndexErr
+	if b.shapesCapacity < i {
+		return nil, errors.New("there are fewer figures in the box than the index")
 	}
-	result := b.shapes[i]
-	b.shapes[i] = shape
-	return result, nil
+	if i >= len(b.shapes) || i < 0 {
+		return nil, errors.New("the element does not exist")
+	} else {
+		result := b.shapes[i]
+		b.shapes[i] = shape
+		return result, nil
+	}
+
 }
 
 // SumPerimeter provides sum perimeter of all shapes in the list.
 func (b *box) SumPerimeter() float64 {
-	var result float64
-	for _, shape := range b.shapes {
-		result += shape.CalcPerimeter()
+	var sumPerimeter float64
+	for _, value := range b.shapes {
+		sumPerimeter += value.CalcPerimeter()
 	}
-	return result
+	return sumPerimeter
 }
 
 // SumArea provides sum area of all shapes in the list.
 func (b *box) SumArea() float64 {
-	var result float64
-	for _, shape := range b.shapes {
-		result += shape.CalcArea()
+	var sumArea float64
+	for _, value := range b.shapes {
+		sumArea += value.CalcArea()
 	}
-	return result
+	return sumArea
 }
 
 // RemoveAllCircles removes all circles in the list
 // whether circles are not exist in the list, then returns an error
 func (b *box) RemoveAllCircles() error {
-	var result []Shape
-	for _, shape := range b.shapes {
-		if _, ok := shape.(*Circle); !ok {
-			result = append(result, shape)
+	var existCirkle bool
+	var newList []Shape
+	for _, value := range b.shapes {
+		a, ok := value.(*Circle)
+		if ok {
+			existCirkle = true
+		} else {
+			newList = append(newList, a)
 		}
+
 	}
-	if len(result) == len(b.shapes) {
-		return errors.New("circles are not exist")
+	if existCirkle {
+		b.shapes = newList
+		return nil
+	} else {
+		return errors.New(" circles are not exist in the list")
 	}
-	b.shapes = result
-	return nil
 }
