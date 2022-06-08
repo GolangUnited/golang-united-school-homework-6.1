8fcf796b6a6fb5f86e39e62dd14b09ca37067947
Anatoliy Dubovik <huykilik@gmail.com>
2022-06-08 09:23:42 +0300

    My Solution v001.1

--- shape.go
+++ shape.go
@@ -1,36 +1,8 @@
 package golang_united_school_homework
 
-import (
-	"math"
-)
-
 type Shape interface {
 	// CalcPerimeter returns calculation result of perimeter
 	CalcPerimeter() float64
 	// CalcArea returns calculation result of area
 	CalcArea() float64
-}
-
-func (c Circle) CalcPerimeter() float64 {
-	return c.Radius * 2 * math.Pi
-}
-
-func (r Rectangle) CalcPerimeter() float64 {
-	return 2 * (r.Height + r.Weight)
-}
-
-func (t Triangle) CalcPerimeter() float64 {
-	return 3 * t.Side
-}
-
-func (c Circle) CalcArea() float64 {
-	return math.Pi * math.Pow(c.Radius, 2)
-}
-
-func (r Rectangle) CalcArea() float64 {
-	return r.Height * r.Weight
-}
-
-func (t Triangle) CalcArea() float64 {
-	return math.Pow(t.Side, 2) * math.Sqrt(3) / 4
 }
